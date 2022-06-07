5a0a6edbe5dc4e0c2cf6050b58b6723eb863db57
Abhinav <rk7772534@gmail.com>
2022-06-07 14:41:34 +0530

    Day 02

---
+++ Arrays/03-next-permutation.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+
+void swap(vector<int> &permutation, int i, int j) {
+    int temp = permutation[i];
+    permutation[i] = permutation[j];
+    permutation[j] = temp;
+}
+
+void reverse(vector<int> &permutation, int i, int j) {
+    while(i < j)
+        swap(permutation, i++, j--);
+}
+
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    if(n<=1)
+         return permutation;   
+    int i = n-2;
+    while(i>=0 && permutation[i] >= permutation[i+1]){
+        i -= 1;
+    }
+    
+    if(i>=0){
+        int j = n-1;
+        while(permutation[j] <= permutation[i]){
+            j -= 1;
+        }
+        swap(permutation, i, j);
+    }
+    reverse(permutation, i+1, n-1);
+    
+    return permutation;
+}
+
+
+int main()
+{
+    int 
+    vector<int> per(2, 3, 1, 4, 5);
+    vector<int> result;
+    int n = 5;
+
+    result = nextPermutation(per, n);
+
+    for(int i=0 ; i<n ; i++){
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
---
+++ Arrays/04-maximum-sum-subarray.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long max_so_far = 0;
+    long long current_max = 0;
+
+    for(int i=0 ; i<n ; i++) {
+        current_max = current_max + arr[i];
+        if(current_max > max_so_far){
+            max_so_far = current_max;
+        }
+        if(current_max < 0){
+            current_max = 0;
+        }
+    }
+    return max_so_far;
+}
+
+int main()
+{
+    int arr[] = {2, 3, -1, 4, -5, 3, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int maxSum = maxSubarraySum();
+    
+    cout << "Max Sum Subarray is: " << maxSum;
+}
